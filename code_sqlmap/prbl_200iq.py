01757cff45f9fa68554e61ac77180742521251b0
quentin-csg <qcassaigne73@gmail.com>
2023-03-16 21:38:41 -0400

    prbl_200iq.py

---
+++ code_sqlmap/prbl_200iq.py
@@ -0,0 +1,8 @@
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","_"]
+
+for i in alphabet:
+    test = "je m'appelle"
+    for i in range(10):
+        test += str(i) + ","
+        print(test)
+    """rajouter lettre alphabet"""
